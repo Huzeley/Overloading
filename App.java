7ce9eca780d39641667c9d85e24f51119cac7a07
Huzeley <106821593+Huzeley@users.noreply.github.com>
2022-06-03 22:04:13 +0800

    Create App.java

---
+++ App.java
@@ -0,0 +1,61 @@
+public class App {
+
+    public static int sum2(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int sum3(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+    private static int computeSum(int num1, int num2){
+        return num1 + num2;
+    }
+
+    private static int computeSum(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+
+
+
+    public static int sum4(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int sum5(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public static int max(int sum4, int sum5){
+        int result;
+        if(sum4 > sum5)
+            result = sum4;
+        else
+            result = sum5;
+
+        return result;
+    }
+
+
+
+
+    public static double sum6(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double sum7(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public static double max(double sum6, double sum7){
+        double result;
+        if(sum6 > sum7)
+            result = sum6;
+        else
+            result = sum7;
+
+        return result;
+    }
+
+}
