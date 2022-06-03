5254ea4d22d42856cd91b5243e22d75a7eddef10
Huzeley <106821593+Huzeley@users.noreply.github.com>
2022-06-03 22:04:48 +0800

    Create appmain.java

---
+++ appmain.java
@@ -0,0 +1,12 @@
+public class appMain {
+    public static void main(String[] args) throws Exception{
+        System.out.println("Hello, Math!");
+
+
+        System.out.println("The sum of 30 and 70 is " + App.sum2(30, 70));
+        System.out.println("The sum of 20 and 50 and 70 is " + App.sum3(20, 50, 70));
+
+        System.out.println("The greater value between 80 and 90 is " + App.max(80, 90));
+        System.out.println("The greater value between 7.19 and 2.78 is " + App.max(7.19, 2.78));
+    }
+}
